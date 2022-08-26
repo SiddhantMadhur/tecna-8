1149a54f2f468aec24658b9822250c00b2def52f
Siddhant Madhur <siddhant.madhur@gmail.com>
2022-08-26 23:37:01 +0530

    init

---
+++ components/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <nav className='box'>
+        <div className='title'>
+            Tecna
+        </div>
+    </nav>
+  )
+}
+
+export default NavBar
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "../styles/navbar.css";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { firebaseConfig } from "../server/firebaseConfig";
+import { useEffect } from "react";
+import NavBar from "../components/navbar";
+
+const app = initializeApp(firebaseConfig);
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    const analytics = getAnalytics(app);
+  }, []);
+  return (
+    <>
+      <NavBar />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
